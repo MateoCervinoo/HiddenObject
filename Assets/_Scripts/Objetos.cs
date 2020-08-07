6b468623e333d993d6db5f20cdffdeeb2b5a738d
Mateo <Cervinomateo@gmail.com>
2020-08-07 11:06:32 -0300

    Initial commit

---
+++ Assets/_Scripts/Objetos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Objetos : MonoBehaviour
+{
+    public GameObject objetoCorrecto;
+    public GameObject objetoIncorrecto;
+
+    void Start()
+    {
+ 
+    }
+
+    void OnMouseDown()
+    {
+        if (objetoCorrecto)
+        {
+            objetoCorrecto.gameObject.SetActive(false);
+            Debug.Log("Bien");
+        }
+        if(objetoIncorrecto)
+        {
+            Debug.Log("Mal");
+        }      
+    }
+}
